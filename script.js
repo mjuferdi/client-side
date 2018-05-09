a1416e80ab46f1a0671fec0eaf4b623c567eeff0
mjuferdi <juferdi.mario@yahoo.com>
2018-05-09 23:34:37 +0200

    merge two json object, nested structure. //TODO comments

--- script.js
+++ script.js
@@ -1,127 +1,37 @@
 $(document).ready(function() {
-  var finalData = [];
   $('#getPost').on('click', function() {
-    //var finalData = [];
-    var commentData = [];
     $.getJSON('https://jsonplaceholder.typicode.com/posts?userId=1', function(data) {
       var postData = data;
-      //console.log(postData);
-      var output = '';
-      var title, body, comments;
-      for (var i = 1; i <= data.length; i++) {
+      var commentData = [];
+      for (var i = 1; i <= postData.length; i++) {
         $.getJSON('https://jsonplaceholder.typicode.com/comments?postId=' + i, function(data) {
-          var commentData = data;
-          console.log(commentData);
-          for(var p in postData){
-            for (var c in commentData){
-              if (commentData[c].postId == postData[p].id) {
-                //finalData = $.extend(postData[p], commentData[c]);
-                //break;
-                finalData = postData.concat(commentData);
+          $.each(data, function(index, value) {
+            commentData.push(value);
+            for (var p in postData) {
+              postData[p].comments = [];
+              for (var c in commentData) {
+                if (postData[p].id == commentData[c].postId) {
+                  postData[p].comments.push(commentData[c]);
+                }
               }
             }
-          }
-          console.log(finalData);
-
+          })
         });
+      };
+
+      //console.log(commentData);
+      var title, body, output = '';
+      for (var i in postData) {
+        title = "<h2>" + postData[i].title + "</h2>" + "<br>";
+        body = "<p>" + postData[i].body + "</p>" + "<br>";
+        //var comments = postData[i].comments;
+        /*for (var j in comments) {
+          var com = comments[j].id;
+        }*/
+        output += title + body + "<hr>";
       }
-      //console.log(postData);
-
+      $('#update').append(output);
+      console.log(postData[1].comments[0].id);
     });
-    // display to html
-    //console.log(finalData);
-    /*var output = '';
-    var title, body, postId, button, comId, comment;
-    postData.forEach(function(itemPost) {
-      postId = itemPost.id;
-      title = "<h2>" + itemPost.title + "</h2>" + "<br>";
-      body = "<h3>" + itemPost.body + "</h3>" + "<br>";
-      button = '<button class="btn btn-primary" id="showComment">' + "Show Comment" + "</button>";
-      commentData.forEach(function(itemComment){
-        comId = "<p>" + itemComment.id + "</p>" + "<br>";
-        comment = "<p>" + itemComment.body + "</p>" + "<br>";
-      });
-    });
-    output += title + body + comId + comment + button + "<hr>";
-
-    $('#update').append(output);*/
-
   });
 });
-
-// callback to process post data
-/*function processPostData(data) {
-  postData = data;
-  //console.log(postData);
-  var output = '';
-  var title, body, id, userId, comments = '';
-  postData.forEach(function(postData) {
-    title = "<h1>" + postData.title + "</h1>" + "<br>";
-    body = '<h3 id="posted">' + postData.body + "</h3>" + "<br>";
-    userId = "<p>" + postData.userId + "</p>";
-    id = "<p>" + postData.id + "</p>";
-    output += title + body + "<hr>";
-  });
-  $('#update').append(output);
-}*/
-
-//callback to process comment data
-/*
-function processCommentData(data) {
-  commentData = data;
-  //console.log(postData);
-  //console.log(commentData);
-  var comments, outputComment = '';
-  postData.forEach(function(postData) {
-    commentData.forEach(function(commentData) {
-      if (postData.id == commentData.postId) {
-        comments += "<p>" + commentData.id + "</p>" + "<br>";
-      }
-    });
-  });
-  outputComment += comments + "<hr>";
-  var output = '';
-  var title, body, id, userId, comments = '';
-  postData.forEach(function(postData) {
-    title = "<h1>" + postData.title + "</h1>" + "<br>";
-    body = '<h3 id="posted">' + postData.body + "</h3>" + "<br>";
-    userId = "<p>" + postData.userId + "</p>";
-    id = "<p>" + postData.id + "</p>";
-    output += title + body + "<hr>";
-  });
-  $('#update').append(output);
-
-  //console.log(output);
-
-  /*postData.forEach(function(postData) {
-    commentData.forEach(function(commentData) {
-      if (postData.id == commentData.postId) {
-        comments += "<p>" + commentData.id + "</p>" + "<br>";
-      }
-    });
-    console.log(comments);
-
-  });*/
-  //$('#update').append(i);
-
-  /*for(var p in postData){
-    for (var c in commentData){
-      if (commentData[c].postId == postData[p].id) {
-        //finalData = $.extend(postData[p], commentData[c]);
-        //break;
-        finalData = postData.concat(commentData);
-      }
-    }
-  }*/
-  //console.log(finalData);
-  /*var output = '';
-  var title, body, id = '';
-  finalData.forEach(function(finalData) {
-    title = "<h2>" + finalData.title + "</h2>" + "<br>";
-    body = "<p>" + finalData.body + "</hp>" + "<br>";
-    button = '<button class="btn btn-primary" id="showComment">' + "Show Comment" + "</button>";
-    output += title + body + button + "<hr>";
-  });
-  //console.log(output);
-  $('#update').append(output);
-}*/
