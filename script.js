29d122396ef6872bd4f037da010d284a7b5b30f5
mjuferdi <juferdi.mario@yahoo.com>
2018-05-10 22:48:31 +0200

    generate fixed //TODO show more comments dynamically

--- script.js
+++ script.js
@@ -31,18 +31,22 @@ function prosesData(data) {
   var title, body, output, btn, totalCom = '';
   title = '<h2 class="title">' + data.title + '</h2>' + "<br>";
   body = '<p class="post">' + data.body + "</p>" + "<br>";
-  Object.keys(data.comments).forEach(function(key) {
-    totalCom += '<p class="comment">' + data.comments[key].body + '</p>' + '<br>';
-    console.log(counter);
-    //console.log(data.comments.length);
-    if (counter > 3 ) {
-      $('.comment').addClass("asd");
+  for (var i = 0; i < data.comments.length; i++) {
+    console.log(data.comments[i]);
+    if (i > 2) {
+      //totalCom += '<p class="comment hide" id=\'c' + i + '\'>' + data.comments[i].body + '</p>';
+      totalCom += '<p class="comment hide">' + data.comments[i].body + '</p>' + '<br>';
+      $('.comment').add;
+    } else {
+      //totalCom += '<p class="comment" id=\'c' + i + '\'>' + data.comments[i].body + '</p>';
+      totalCom += '<p class="comment">' + data.comments[i].body + '</p>' + '<br>';
     }
+    $('.btnCom').on('click', function() {
+      $('.comment').removeClass('hide');
+    });
+    btn = '<button class="btn btn-primary btnCom">' + "Show more comments" + '</button>';
     counter++;
-    console.log(counter);
-  })
-  btn = '<button class="btn btn-primary btnCom">' + "Show more comments" + '</button>';
-  //console.log(com);
+  }
   output = title + body + totalCom + btn + '<hr style="margin-top:50px">';
 
   $('#update').append(output);
